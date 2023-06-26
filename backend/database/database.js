1ee6c2ab1be024438da72d2a57d267df034c576e
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2023-06-26 22:46:40 +0700

    switch pool database connection

---
+++ backend/database/database.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const connectionConfig = {
+  database: process.env.DATABASE,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+}
+
+const pool = mysql.createPool({
+  ...connectionConfig,
+  connectionLimit: 15,
+  ssl: {
+    "rejectUnauthorized": true,
+  }
+})
+
+exports.pool = pool;
