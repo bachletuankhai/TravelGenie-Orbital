1f80e5ec2b19e5297d779c6597c3728242b6e815
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2023-07-12 20:48:19 +0700

    add listener to location change

--- TravelGenie/contexts/homeLocation.js
+++ TravelGenie/contexts/homeLocation.js
@@ -11,10 +11,10 @@ export function useHomeLocationContext() {
 export function LocationProvider({ children }) {
   const [location, setLocation] = useState(null);
   const [address, setAddress] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
+      console.log('first time request for location');
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         return;
@@ -24,25 +24,42 @@ export function LocationProvider({ children }) {
 
       try {
         // reverse geocoding to set address
-        const response = await axios.get(
-            `https://api.geoapify.com/v1/geocode/reverse`,
-            {
-              params: {
-                lat: location.coords.latitude,
-                lon: location.coords.longitude,
-                type: "city",
-                lang: 'en',
-                format: "json",
-                apiKey: process.env.GEOAPIFY_API_KEY,
-              },
-            },
-        );
-        // console.log(response.data?.results[0]);
-        setAddress(response.data?.results[0]);
+        // TODO: uncomment this
+        // const response = await axios.get(
+        //     `https://api.geoapify.com/v1/geocode/reverse`,
+        //     {
+        //       params: {
+        //         lat: location.coords.latitude,
+        //         lon: location.coords.longitude,
+        //         type: "city",
+        //         lang: 'en',
+        //         format: "json",
+        //         apiKey: process.env.GEOAPIFY_API_KEY,
+        //       },
+        //     },
+        // );
+        // // console.log(response.data?.results[0]);
+        // setAddress(response.data?.results[0]);
       } catch (error) {
         console.log(error);
       }
     })();
+  }, []);
+
+  // subscribe to location change
+  useEffect(() => {
+    const id = setInterval(async () => {
+      if (flag) {
+        const location = await Location.getLastKnownPositionAsync({});
+        setLocation(location);
+      }
+    }, 60000);
+
+    return () => {
+      console.log('remove subscription');
+      flag = false;
+      clearInterval(id);
+    };
   }, []);
 
 
