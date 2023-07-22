69cc8c4871a99e08ae8008f383d471be04e8dbfc
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2023-07-22 18:50:10 +0700

    add create plan item function

--- TravelGenie/lib/itinerary.js
+++ TravelGenie/lib/itinerary.js
@@ -97,3 +97,67 @@ export async function createItinerary(
     return { error };
   }
 }
+
+export async function createPlanItem(
+    planId,
+    name,
+    subtitle,
+    date,
+    startTime,
+    endTime,
+    placeId,
+) {
+  const api = "/itinerary/items";
+  console.log(api);
+  console.log("id: " + planId);
+  console.log("name: " + name);
+  console.log("subtitle: " + subtitle);
+  console.log('date: ' + date);
+  console.log('startTime' + startTime);
+  console.log('endTime: ' + endTime);
+  console.log('placeId: ' + placeId);
+  try {
+    const res = await axios.post(
+        api,
+        {
+          name,
+          subtitle,
+          date,
+          startTime,
+          endTime,
+          placeId,
+        },
+        {
+          headers: {
+            'x-api-key': process.env.BACKEND_API_KEY,
+          },
+          params: {
+            'id': planId,
+          },
+          timeout: 10000,
+          timeoutErrorMessage: "Network Error",
+        });
+    if (res.data.error) {
+      throw new Error(error);
+    } else {
+      return res.data;
+    }
+  } catch (error) {
+    console.log(`createItinerary error: ${error}`);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      return error.response.data;
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    return { error };
+  }
+}
