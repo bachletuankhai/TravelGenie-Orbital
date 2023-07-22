6302220e6ffb695ab2aeea3a215c85a468c2e965
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2023-07-22 18:52:33 +0700

    change datestring format to YYYY-MM-DD

--- TravelGenie/components/ItineraryPage/NewTrip.jsx
+++ TravelGenie/components/ItineraryPage/NewTrip.jsx
@@ -225,8 +225,10 @@ function DatePickModalIOS({
 
 function toDateString(date) {
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  let month = date.getMonth() + 1;
+  if (month < 10) month = '0' + month;
+  let day = date.getDate();
+  if (day < 10) day = '0' + day;
   return `${year}-${month}-${day}`;
 }
 
